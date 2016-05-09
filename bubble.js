b0d359d964766880b245e6111623463eb4da5038
Megan Hodge <mhodge16@uw.edu>
2016-05-09 08:17:35 -0700

    grab is pre-existing and allows dynamic objects to be dragged, however adding dynamic to the bubbles makes them start with a velocity and continue to move when dragged because no gravity

--- bubble.js
+++ bubble.js
@@ -3,10 +3,21 @@ function addBubble() {
 	// TODO: determine bubble's distance away from the avatar's face
 	var height = MyAvatar.getDefaultEyePosition();
 	var properties = {
-	  type: "Sphere",
-	  position: height,
-	  // attaches the color changing script
-	  script: Script.resolvePath("changeColorOnClickDown.js")
+		type: "Sphere",
+		position: height,
+		// attaches the color changing script
+		script: Script.resolvePath("changeColorOnClickDown.js"),
+		dynamic: true,
+		// velocity: {
+		// 	x: 0,
+		// 	y: 0,
+		// 	z: 0
+		// },
+		// gravity: {
+		// 	x: 0,
+		// 	y: 0,
+		// 	z: -1
+		// }
 	};
 
 	var bubble = Entities.addEntity(properties);
