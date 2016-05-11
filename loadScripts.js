0c497b730be5b431fc81885470749c6d63316a5d
Megan Hodge <mhodge16@uw.edu>
2016-05-10 23:52:39 -0700

    loads scripts, TODO figure out grab file issue

---
+++ loadScripts.js
@@ -0,0 +1,6 @@
+//	load scripts
+
+Script.load("bubble.js");
+Script.load("changeColorOnClickDown.js");
+//	figure out why loading grab.js from another folder outside of 
+//	High Fidelity app folder doesn't work
